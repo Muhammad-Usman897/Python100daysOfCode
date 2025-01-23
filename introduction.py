7a56bb90ecb3269b5f35eb7858b8c8edd6897726
Muhammad Usman <muh.musman@gmail.com>
2025-01-23 20:59:28 +0500

    intruduction

---
+++ introduction.py
@@ -0,0 +1,17 @@
+#What is programming?
+# like We use Hindi or English to communicate with each other, we use a programming language like Python the computer to Communicate with Programming is a way to instrud the computer to perform various tasks.
+
+#What is Python? 
+#Python a simble and language which feels easy to understand like reading english This Pseudo Code nature it easy and understandable learn and Simple python mak by beginners
+
+#Features of Python 
+# Easy to understand
+# Free and open source = Less development time
+# High level language
+# Portable → Works on Linux/Windows/Mac
+# +fun to work with.
+
+
+#Python is a dynamically object oriented type Programming Language
+#Which was made by Guido van Russom in 1991
+print('Muhammad Usman');
