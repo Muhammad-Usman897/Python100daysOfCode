b6338c9034bda1bb392a0d08ac207126257feda8
Muhammad Usman <muh.musman@gmail.com>
2025-01-25 23:56:44 +0500

    commentsAndEscapeChar

---
+++ commentsAndEscapeChar.py
@@ -0,0 +1,13 @@
+# --------------------COMMENTS----------------------
+# A Comment is a part of coding file that the programmer does not want execute
+# --------------------TYPES OF COMMENTS----------------------
+# ........ Single-Line Comments ......
+# This is Single-Line Commnets
+# ........ Multi-Line Comments .......
+'''
+This is Multi-line Comments
+Yes, this is multi-line Comments
+'''
+
+
+print("Hey I am good boy \nand this viewr also a good boy/girl")
