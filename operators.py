ddc600fa44597cb929cf19537e4429483265de5b
Muhammad Usman <muh.musman@gmail.com>
2025-02-01 22:16:01 +0500

    operators

---
+++ operators.py
@@ -0,0 +1,30 @@
+#........... Arithmatic Operators ..............
+print(15+6) #Additon
+print(15-6) #Substraction
+print(15*6) #Multiplication
+print(15/6) #Division
+print(15//6) #Floor Division Operator
+print(15%6) #Module Operator
+print(5**3) #Exponential Operator
+
+
+# ..............CALCULATOR................
+num1=15
+num2=6
+ans=num1+num2
+print('Addition of', num1, 'and', num2, 'is:', ans)
+
+ans=num1-num2
+print('Subtraction of', num1, 'and', num2, 'is:', ans)
+
+ans=num1*num2
+print('Multiplication of', num1, 'and', num2, 'is:', ans)
+
+ans=num1/num2
+print('Division of', num1, 'and', num2, 'is:', ans)
+
+ans=num1//num2
+print('Floor Value After Divide of', num1, 'and', num2, 'is:', ans)
+
+ans=num1%num2
+print('Modulus of', num1, 'and', num2, 'is:', ans)
