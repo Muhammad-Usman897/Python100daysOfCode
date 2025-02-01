e34ed7116dc97d1723f1b1d81d2b50a18f86f532
Muhammad Usman <muh.musman@gmail.com>
2025-02-01 22:47:34 +0500

    typecasting

---
+++ typeCasting.py
@@ -0,0 +1,13 @@
+#The Conversion of one data into the other data type is known as typecasting in python or type conversion in python
+
+# ... Explicit TypeCasting ...
+string='15'
+number=7
+string_number=int(string)
+sum=number+string_number
+print('The Sum of both these number is:', sum)
+
+# ... Implicit TypeCasting ...
+a=6.6
+b=5
+print(a+b)
---
+++ variablesAndDatatypes.py
@@ -0,0 +1,13 @@
+#Variables is like a container that hold data
+#Data type specifies the type of vlaue a variables hold
+
+a=complex(2,3);
+b="Usman"
+c=True;
+print(a)
+print(b)
+print(c)
+
+print('The type of a is', type(a))
+print('The type of a is', type(b))
+print('The type of a is', type(c))
