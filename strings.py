f9e5a6f3ee7d91df023b9a510d1e1d482c3ececf
Muhammad Usman <muh.musman@gmail.com>
2025-02-02 12:57:35 +0500

    strings

---
+++ strings.py
@@ -0,0 +1,21 @@
+name='Usman'
+Brother='Furqan'
+
+print('Hey,', name)
+
+# Single-Line String
+apple='He Said, "I want to eat an apple'
+print(apple)
+
+# Multi-Line String
+Mulstr="""Hey, I'm Muhammad Usman
+and am Pursuing Bachelor Degree in Computer Science
+from UET Lahore"""
+print(Mulstr)
+
+
+# Accessing Characters in String
+str='Usman'
+print([0])
+print([1])
+print([2])
