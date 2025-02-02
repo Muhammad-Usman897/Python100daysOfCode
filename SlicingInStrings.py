09a79a488a3da28e20a3b9ced9494664fe2dd182
Muhammad Usman <muh.musman@gmail.com>
2025-02-02 14:45:18 +0500

    slicingInString

---
+++ SlicingInStrings.py
@@ -0,0 +1,13 @@
+# ......SLICING.....
+name='MuhammadUsman'
+print(name[0:13])
+print(name[8:13])
+
+nm='Usman'
+print(nm[-4:-2])
+
+# Find the length of the String
+fruit='Mango'
+len1=len(fruit)
+print('Mango is a', len1, 'Letter Word.')
+
