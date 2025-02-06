631e6b410388ec0dbd99f744727f9be3a71fedcb
Muhammad Usman <muh.musman@gmail.com>
2025-02-06 23:33:30 +0500

    WhileLoop

---
+++ whileLoop.py
@@ -0,0 +1,21 @@
+# While Loop in python
+i=0;
+while(i<5):
+    print(i)
+    i=i+1;
+    
+ 
+# x=int(input('Enter the number'))
+# while(x<50):
+#   x=int(input('Enter the number'))
+#   print(x)
+  
+# Else with while loop
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print('i am inside else')
+  
+    
