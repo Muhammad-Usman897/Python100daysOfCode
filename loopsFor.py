7111573e2d870618e023a2b5197170200d96a998
Muhammad Usman <muh.musman@gmail.com>
2025-02-06 22:58:38 +0500

    forLoop

---
+++ loopsFor.py
@@ -0,0 +1,24 @@
+# For loop can iterate over a sequence of iterable objects in python
+# name='Usman'
+# for i in name:
+#     print(i, end=", ")
+#     if(i=="m"):
+#         print('THis is something special')
+        
+# colors=['red', 'green', 'yellow', 'blue']
+# for color in colors:
+#     print(color)
+#     for x in color:
+#         print(x)
+        
+ 
+ 
+for i in range(10):
+    print(i)
+    
+for i in range(1, 5):
+    print(i)
+    
+for i in range(1, 20,3):
+    print(i)
+        
