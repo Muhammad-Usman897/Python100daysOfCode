a6b8f52589c073246c88c622d191ad4ec79385bd
Muhammad Usman <muh.musman@gmail.com>
2025-02-08 11:42:05 +0500

    breakContinue1

---
+++ breakContinue.py
@@ -0,0 +1,10 @@
+# for i in range(10):
+#     print('5 *', i+1, '=', 5*(i+1))
+#     if(i==5):
+#         break #Break statement used to break the whole loop
+    
+for i in range(10):
+    if(i==5):
+        continue #continue statement used to skip the iteration     
+    print('5 *', i+1, '=', 5*(i+1))
+    
