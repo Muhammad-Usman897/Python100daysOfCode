0e8564ff1057f45b60a198afac76932a2fc1936a
Muhammad Usman <muh.musman@gmail.com>
2025-02-08 12:10:16 +0500

    function

---
+++ functions.py
@@ -0,0 +1,10 @@
+# ......... FUNCTIONS ............
+# A function is a block of code that perform a specific task
+def calculategmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+calculategmean(6,8)
+calculategmean(9,81)
+
+def isGreater():
+    pass
