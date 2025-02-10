b26203d90d9b689dfe7566c8ec3cab104401caf7
Muhammad Usman <muh.musman@gmail.com>
2025-02-10 20:07:51 +0500

    sets

---
+++ sets.py
@@ -0,0 +1,17 @@
+# ----------------- SETS -------------------
+# * sets are unordered collection of data items
+# * They are store multiple items in a single variable and enclosed with a {} curly brackets
+# * sets are immutable, sets do not contain duplicate items
+
+s={2,4,2,6}
+print(s)
+print(type(s))
+
+info={'Car', 21, 5.68, 21}
+print(info)
+
+for value in info:
+    print(value)
+
+usman=set()
+print(type(usman))
