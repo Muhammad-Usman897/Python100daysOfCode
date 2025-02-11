16f3b24df6107f096c7e69e8e633f910d8689fe4
Muhammad Usman <muh.musman@gmail.com>
2025-02-11 23:07:03 +0500

    dictMethod

---
+++ dictMethod.py
@@ -0,0 +1,25 @@
+# -------- DICTONARIES METHODS ----------
+ep1={
+    520: 'Syed Abdul Rehman',
+    566: 'Muhammad Aamir',
+    568: 'Muhammad Usman'
+}
+
+ep2={
+    518: 'Abdullah Mumtaz',
+    569: 'Muhammad Awais'
+}
+
+# ep1.update(ep2)
+# print(ep1)
+
+# ep1.clear()
+# print(ep1)
+
+# ep1.pop(566)
+# print(ep1)
+
+# ep1.popitem(566) #remove the last key-pair value
+# print(ep1)
+
+del ep1
